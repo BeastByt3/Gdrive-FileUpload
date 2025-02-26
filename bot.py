2257d27ea5ff3df4bf608c34e2dcabc4ac2bc8e0
BeastByt3 <124415177+BeastByt3@users.noreply.github.com>
2025-02-26 09:10:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-VERIFY_TOKEN = "your_verify_token"
+VERIFY_TOKEN = "my4psbot123"
 PAGE_ACCESS_TOKEN = "EAANlu9ZCmDo0BOZBrNQhbfxkSYd4MiJrrVBeZBDifOM9gQhTHqz5R3IVpsrk0Uv8oEyFUVx3tisGZARE0druzx2KNZA9VYjuUVJn6xvN4VkvCXhyEbAIv66wyUlAQy9D1ZCkfecBvBjz99qZA6AtGbTyvGttqYud2G7XSKV66e9SgRxurpF8AFtIsPZBknM9x3Pt"
 
 @app.route("/", methods=["GET"])
