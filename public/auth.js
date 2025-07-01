286377658ce17de2666409b92da0e6d7d93213f3
BeastByt3 <124415177+BeastByt3@users.noreply.github.com>
2025-07-01 18:17:34 +0800

    Delete public directory

--- public/auth.js
+++
@@ -1,9 +0,0 @@
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyA-hORXx-5pHAN_JGnoD7Ll4K8XcMC2_fc",
-    authDomain: "casemanagement-project.firebaseapp.com",
-    projectId: "casemanagement-project",
-    appId: "casemanagement-project" // optional but better to include
-  };
-  firebase.initializeApp(firebaseConfig);
-</script>
